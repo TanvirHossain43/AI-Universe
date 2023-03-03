6f1d75f3f93e2bc28d2be322538b16f4e3b523e3
Tanvir hossain <tanvirhossain4393@gamil.com>
2023-03-03 13:06:25 +0600

    information cards are added individuallay from api

--- js/api.js
+++ js/api.js
@@ -3,6 +3,7 @@ const loadApiData =() =>{
     fetch(url)
     .then(res => res.json())
     .then(data => displayApiData(data.data.tools))
+ 
 }
 const displayApiData =(tools) =>{
 // console.log(data.tools[0].image)
@@ -14,20 +15,35 @@ const dataDiv =document.createElement('div')
 dataDiv.classList.add('col')
 dataDiv.innerHTML =`
             <div class="card h-100">
-                <img src="${tools.image}" class="card-img-top" alt="...">
+                <img src="${tools.image}" style ="height:200px" class="card-img-top img-fluid" alt="...">
                 
-                <div class="card-body">
+                <div class="">
                     <h5 class="card-title">Features</h5>
                     <p class="card-text">1.${tools.features[0]}</p>
                     <p class="card-text">2.${tools.features[1]}</p>
                     <p class="card-text">3.${tools.features[2]}</p>
+                    
+                    <hr>
                    
                 </div>
+               
+                <div class="mt-0 px-3 d-flex align-items-center justify-content-between">
+               <div> 
+               <h4>${tools.name}</h4>
+               <p>${tools.published_in}</P>
+               </div>
+               <div>
+               <p>&#8594;</P>
+               </div>
+              </div>
             </div>
 `
 dataContainer.appendChild(dataDiv)
     
 });
 }
+
+
+
 
 loadApiData()
