ef56314a67721780bfe7abf311b1cb759c436928
Tanvir hossain <tanvirhossain4393@gamil.com>
2023-03-04 00:41:29 +0600

    added fetch link of a single data

--- js/api.js
+++ js/api.js
@@ -34,7 +34,7 @@ const displayApiData = (tools) => {
                <p>${tools.published_in}</P>
                </div>
                <div>
-               <button onclick ="apiDetails(${tools.id})"  id="details-btn" class ="bg-warning-subtle text-danger fs-2 px-2 border-0 rounded-circle" type="button" data-bs-toggle="modal" data-bs-target="#apiDetailsModal">&#8594;</button>
+               <button onclick ="apiDetails('${tools.id}')"  id="details-btn" class ="bg-warning-subtle text-danger fs-2 px-2 border-0 rounded-circle" type="button" data-bs-toggle="modal" data-bs-target="#apiDetailsModal">&#8594;</button>
                </div>
               </div>
             </div>
@@ -45,13 +45,54 @@ const displayApiData = (tools) => {
     });
 }
 
-const apiDetails = apiId => {
-    const url = `https://openapi.programming-hero.com/api/ai/tool/${apiId}`
+const apiDetails = id => {
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data.data.id))
+        .then(data => displaySingleApiData (data))
 }
-
+ const displaySingleApiData = (data) =>{
+   const modalBodyDetails =document.getElementById('modal-body')
+   const modalLeftDetails =document.getElementById('modal-left-details')
+   modalLeftDetails.innerHTML =`
+        <p>${data.data.description}</p>
+        <div class="row column-gap-3 px-3">
+           <div class ="col bg-light-subtle">
+           <p>${data.data.pricing[0].price} <br>${data.data.pricing[0].plan}</p>
+           </div>
+           <div class ="col bg-light-subtle">
+           <p>${data.data.pricing[1].price} <br>${data.data.pricing[1].plan}</p>
+           </div>
+           <div class ="col bg-light-subtle">
+           <p>${data.data.pricing[2].price} <br>${data.data.pricing[2].plan}</p>
+           </div>
+        </div>
+        <div>
+             <div>
+                 <h3>Features</h3>
+                 
+                  <p>2.${data.data.features[1].feature_name}</p>
+                  <p>3.${data.data.features[2].feature_name}</p>
+                  <p>1.${data.data.features[3].feature_name}</p>
+                  
+             </div>
+             <div>
+             
+             </div>
+        </div>
+   
+   `
+   const modalRightDetails =document.getElementById('modal-right-details')
+   modalRightDetails.innerHTML =`
+   <div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+</div>
+   
+   `
+ }
 
 loadApiData()
 
